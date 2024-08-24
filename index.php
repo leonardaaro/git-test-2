2fa59fc96d23f48dc3bff2c9a95e3add9c0c3867
Leonard Kumaro <kumaro.2208541041@student.unud.ac.id>
2024-08-24 11:27:11 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <p>Hello World</p>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste maiores veniam inventore itaque odit voluptatibus praesentium enim natus officiis, repudiandae, doloribus delectus labore excepturi iure minus molestias ratione iusto! Iusto?</p>
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
 </body>
 </html>
